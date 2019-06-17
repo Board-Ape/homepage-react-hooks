3e5f2bc6d27adda75cdc4568450911e46b7f4693
Samuel <samuel_singer@apple.com>
2019-06-17 15:34:47 -0700

    When redirecting with target. Use rel noopener nonreferrer

--- src/index.js
+++ src/index.js
@@ -4,21 +4,3 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-function createObject() {
-    console.log('outermost this', this);
-
-    return {
-        arrowFunction: () => {
-            console.log('arrowFunction this', this);
-        },
-        functionKeywordFunction: function() {
-            console.log('functionKeyword this', this);
-        }
-    }
-}
-
-const obj = createObject()
-console.log('object', obj)
-obj.arrowFunction()
-obj.functionKeywordFunction() 
